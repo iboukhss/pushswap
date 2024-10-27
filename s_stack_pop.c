52ef58eb0ab63ce6f94daf5a39cb7dfdd60307dd
Ismail <iboukhss.student@42luxembourg.lu>
2024-10-27 12:33:15 +0100

    unify style

--- s_stack_pop.c
+++ s_stack_pop.c
@@ -6,7 +6,7 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 01:12:53 by iboukhss          #+#    #+#             */
-/*   Updated: 2024/10/26 02:28:43 by iboukhss         ###   ########.fr       */
+/*   Updated: 2024/10/27 12:31:49 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,14 @@ int	stack_a_pop_back(int *out_val, t_stack *stack)
 	ptrdiff_t	move_size;
 
 	if (stack_a_is_empty(stack))
+	{
 		return (1);
+	}
 	if (out_val != NULL)
+	{
 		*out_val = *(stack->end_a - 1);
-	move_size = (stack->end_b - stack->end_a) * sizeof(int);
+	}
+	move_size = (stack->end_b - stack->end_a) * sizeof(*stack->data);
 	memmove(stack->end_a - 1, stack->end_a, move_size);
 	*(stack->end_b - 1) = 0;
 	stack->end_a -= 1;
@@ -40,10 +44,14 @@ int	stack_a_pop_front(int *out_val, t_stack *stack)
 	ptrdiff_t	move_size;
 
 	if (stack_a_is_empty(stack))
+	{
 		return (1);
+	}
 	if (out_val != NULL)
+	{
 		*out_val = *(stack->beg_a);
-	move_size = (stack->end_b - stack->beg_a - 1) * sizeof(int);
+	}
+	move_size = (stack->end_b - stack->beg_a - 1) * sizeof(*stack->data);
 	memmove(stack->beg_a, stack->beg_a + 1, move_size);
 	*(stack->end_b - 1) = 0;
 	stack->end_a -= 1;
@@ -55,9 +63,13 @@ int	stack_a_pop_front(int *out_val, t_stack *stack)
 int	stack_b_pop_back(int *out_val, t_stack *stack)
 {
 	if (stack_b_is_empty(stack))
+	{
 		return (1);
+	}
 	if (out_val != NULL)
+	{
 		*out_val = *(stack->end_b - 1);
+	}
 	*(stack->end_b - 1) = 0;
 	stack->end_b -= 1;
 	return (0);
@@ -68,10 +80,14 @@ int	stack_b_pop_front(int *out_val, t_stack *stack)
 	ptrdiff_t	move_size;
 
 	if (stack_b_is_empty(stack))
+	{
 		return (1);
+	}
 	if (out_val != NULL)
+	{
 		*out_val = *(stack->beg_b);
-	move_size = (stack->end_b - stack->beg_b - 1) * sizeof(int);
+	}
+	move_size = (stack->end_b - stack->beg_b - 1) * sizeof(*stack->data);
 	memmove(stack->beg_b, stack->beg_b + 1, move_size);
 	*(stack->end_b - 1) = 0;
 	stack->end_b -= 1;
--- s_stack_push.c
+++ s_stack_push.c
@@ -6,7 +6,7 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 20:12:25 by iboukhss          #+#    #+#             */
-/*   Updated: 2024/10/26 02:04:29 by iboukhss         ###   ########.fr       */
+/*   Updated: 2024/10/27 12:30:10 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ void	stack_a_push_back(t_stack *stack, int value)
 	ptrdiff_t	move_size;
 
 	if (stack_is_full(stack))
+	{
 		return ;
-	move_size = (stack->end_b - stack->beg_b) * sizeof(int);
+	}
+	move_size = (stack->end_b - stack->beg_b) * sizeof(*stack->data);
 	memmove(stack->beg_b + 1, stack->beg_b, move_size);
 	*(stack->end_a) = value;
 	stack->end_a += 1;
@@ -33,8 +35,10 @@ void	stack_a_push_front(t_stack *stack, int value)
 	ptrdiff_t	move_size;
 
 	if (stack_is_full(stack))
+	{
 		return ;
-	move_size = (stack->end_b - stack->beg_a) * sizeof(int);
+	}
+	move_size = (stack->end_b - stack->beg_a) * sizeof(*stack->data);
 	memmove(stack->beg_a + 1, stack->beg_a, move_size);
 	*(stack->beg_a) = value;
 	stack->end_a += 1;
@@ -45,7 +49,9 @@ void	stack_a_push_front(t_stack *stack, int value)
 void	stack_b_push_back(t_stack *stack, int value)
 {
 	if (stack_is_full(stack))
+	{
 		return ;
+	}
 	*(stack->end_b) = value;
 	stack->end_b += 1;
 }
@@ -55,8 +61,10 @@ void	stack_b_push_front(t_stack *stack, int value)
 	ptrdiff_t	move_size;
 
 	if (stack_is_full(stack))
+	{
 		return ;
-	move_size = (stack->end_b - stack->beg_b) * sizeof(int);
+	}
+	move_size = (stack->end_b - stack->beg_b) * sizeof(*stack->data);
 	memmove(stack->beg_b + 1, stack->beg_b, move_size);
 	*(stack->beg_b) = value;
 	stack->end_b += 1;
