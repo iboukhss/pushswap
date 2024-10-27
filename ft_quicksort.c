565d6784f37a852888d308e77a731a402764b084
Ismail <iboukhss.student@42luxembourg.lu>
2024-10-27 22:53:15 +0100

    going off (wip)

---
+++ ft_quicksort.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_quicksort.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 21:46:44 by iboukhss          #+#    #+#             */
+/*   Updated: 2024/10/27 22:09:09 by iboukhss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_main.h"
+
+static void	swap(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+// NOTE: Picking last index as pivot is a classic choice for quicksort.
+static ptrdiff_t	partition(int *array, ptrdiff_t low, ptrdiff_t high)
+{
+	int			p;
+	ptrdiff_t	i;
+	ptrdiff_t	j;
+
+	p = array[high];
+	i = low - 1;
+	j = low;
+	while (j < high)
+	{
+		if (array[j] <= p)
+		{
+			++i;
+			swap(&array[i], &array[j]);
+		}
+		++j;
+	}
+	swap(&array[i + 1], &array[high]);
+	return (i + 1);
+}
+
+static void	quicksort_recursive(int *array, ptrdiff_t low, ptrdiff_t high)
+{
+	ptrdiff_t	pi;
+
+	if (low >= high)
+	{
+		return ;
+	}
+	pi = partition(array, low, high);
+	quicksort_recursive(array, low, pi - 1);
+	quicksort_recursive(array, pi + 1, high);
+}
+
+// NOTE: We pass the min and max indices to our recursive function.
+void	ft_quicksort(int *array, ptrdiff_t array_size)
+{
+	quicksort_recursive(array, 0, array_size - 1);
+}
--- ps_main.c
+++ ps_main.c
@@ -6,6 +6,42 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 19:45:25 by iboukhss          #+#    #+#             */
-/*   Updated: 2024/10/25 19:45:25 by iboukhss         ###   ########.fr       */
+/*   Updated: 2024/10/27 17:49:51 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ps_main.h"
+#include "dbg_main.h"
+
+int	main(void)
+{
+	int nums[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+	t_stack *stk = stack_new(nums, 9);
+	t_state s;
+	state_init(&s, stk, TOP_A, stk->end_a - stk->beg_a);
+	state_print(&s);
+	while (!chunk_is_empty(&s.curr))
+	{
+		if (chunk_peek(&s.curr) <= 3)
+			state_push_min(&s);
+		else if (chunk_peek(&s.curr) <= 6)
+			state_push_mid(&s);
+		else
+			state_push_max(&s);
+	}
+	state_print(&s);
+
+	state_init(&s, stk, s.max.pos, s.max.end - s.max.beg);
+	state_print(&s);
+	while (!chunk_is_empty(&s.curr))
+	{
+		if (chunk_peek(&s.curr) <= 7)
+			state_push_min(&s);
+		else if (chunk_peek(&s.curr) <= 8)
+			state_push_mid(&s);
+		else
+			state_push_max(&s);
+	}
+	state_print(&s);
+	stack_delete(stk);
+}
