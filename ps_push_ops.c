bc634baa2531195905d68784db3a91ca1c15f8a3
Ismail <iboukhss.student@42luxembourg.lu>
2024-11-02 02:08:02 +0100

    simplified main push_swap operations

--- ps_push_ops.c
+++ ps_push_ops.c
@@ -6,7 +6,7 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 01:01:48 by iboukhss          #+#    #+#             */
-/*   Updated: 2024/10/31 00:26:35 by iboukhss         ###   ########.fr       */
+/*   Updated: 2024/11/01 22:00:23 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,24 @@
 
 #include <stdio.h>
 
-// NOTE: Error checking could be improved. Pop operations need to be handled
-// very carefully (nasty bugs).
-
 void	push_a(t_state *state)
 {
-	int		val;
-	bool	err;
+	int	val;
 
-	err = false;
-	err |= stack_is_empty_b(state->stack);
-	err |= chunk_is_empty(chunk_at(state, TOP_B));
-	if (!err)
-	{
-		stack_pop_front_b(&val, state->stack);
-		chunk_pop(state, TOP_B);
-		stack_push_front_a(state->stack, val);
-		chunk_push(state, TOP_A);
-		puts("pa");
-	}
+	val = stack_pop_front_b(state->stack);
+	stack_push_front_a(state->stack, val);
+	chunk_pop(state, TOP_B);
+	chunk_push(state, TOP_A);
+	puts("pa");
 }
 
 void	push_b(t_state *state)
 {
-	int		val;
-	bool	err;
+	int	val;
 
-	err = false;
-	err |= stack_is_empty_a(state->stack);
-	err |= chunk_is_empty(chunk_at(state, TOP_A));
-	if (!err)
-	{
-		stack_pop_front_a(&val, state->stack);
-		chunk_pop(state, TOP_A);
-		stack_push_front_b(state->stack, val);
-		chunk_push(state, TOP_B);
-		puts("pb");
-	}
+	val = stack_pop_front_a(state->stack);
+	stack_push_front_b(state->stack, val);
+	chunk_pop(state, TOP_A);
+	chunk_push(state, TOP_B);
+	puts("pb");
 }
--- ps_rotate_ops.c
+++ ps_rotate_ops.c
@@ -6,7 +6,7 @@
 /*   By: iboukhss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 02:56:31 by iboukhss          #+#    #+#             */
-/*   Updated: 2024/10/31 00:03:22 by iboukhss         ###   ########.fr       */
+/*   Updated: 2024/11/01 22:04:15 by iboukhss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,72 +16,44 @@
 
 void	rotate_a(t_state *state)
 {
-	int		val;
-	bool	err;
+	int	val;
 
-	err = false;
-	err |= stack_is_empty_a(state->stack);
-	err |= chunk_is_empty(chunk_at(state, TOP_A));
-	if (!err)
-	{
-		stack_pop_front_a(&val, state->stack);
-		chunk_pop(state, TOP_A);
-		stack_push_back_a(state->stack, val);
-		chunk_push(state, BOT_A);
-		puts("ra");
-	}
+	val = stack_pop_front_a(state->stack);
+	stack_push_back_a(state->stack, val);
+	chunk_pop(state, TOP_A);
+	chunk_push(state, BOT_A);
+	puts("ra");
 }
 
 void	rotate_b(t_state *state)
 {
-	int		val;
-	bool	err;
+	int	val;
 
-	err = false;
-	err |= stack_is_empty_b(state->stack);
-	err |= chunk_is_empty(chunk_at(state, TOP_B));
-	if (!err)
-	{
-		stack_pop_front_b(&val, state->stack);
-		chunk_pop(state, TOP_B);
-		stack_push_back_b(state->stack, val);
-		chunk_push(state, BOT_B);
-		puts("rb");
-	}
+	val = stack_pop_front_b(state->stack);
+	stack_push_back_b(state->stack, val);
+	chunk_pop(state, TOP_B);
+	chunk_push(state, BOT_B);
+	puts("rb");
 }
 
 void	reverse_rotate_a(t_state *state)
 {
-	int		val;
-	bool	err;
+	int	val;
 
-	err = false;
-	err |= stack_is_empty_a(state->stack);
-	err |= chunk_is_empty(chunk_at(state, BOT_A));
-	if (!err)
-	{
-		stack_pop_back_a(&val, state->stack);
-		chunk_pop(state, BOT_A);
-		stack_push_front_a(state->stack, val);
-		chunk_push(state, TOP_A);
-		puts("rra");
-	}
+	val = stack_pop_back_a(state->stack);
+	stack_push_front_a(state->stack, val);
+	chunk_pop(state, BOT_A);
+	chunk_push(state, TOP_A);
+	puts("rra");
 }
 
 void	reverse_rotate_b(t_state *state)
 {
-	int		val;
-	bool	err;
+	int	val;
 
-	err = false;
-	err |= stack_is_empty_b(state->stack);
-	err |= chunk_is_empty(chunk_at(state, BOT_B));
-	if (!err)
-	{
-		stack_pop_back_b(&val, state->stack);
-		chunk_pop(state, BOT_B);
-		stack_push_front_b(state->stack, val);
-		chunk_push(state, TOP_B);
-		puts("rrb");
-	}
+	val = stack_pop_back_b(state->stack);
+	stack_push_front_b(state->stack, val);
+	chunk_pop(state, BOT_B);
+	chunk_push(state, TOP_B);
+	puts("rrb");
 }
